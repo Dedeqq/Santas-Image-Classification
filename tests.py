5bf41f2c5970c4f3dd712230874218d80a5de3d3
Lukasz Chudy <Lukain2709@gmail.com>
2022-04-06 18:43:42 +0200

    More tests

---
+++ tests.py
@@ -0,0 +1,148 @@
+#%% Liblaries
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow.keras
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout 
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+
+from sklearn.metrics import classification_report,confusion_matrix
+
+import tensorflow as tf
+
+import cv2
+import os
+
+import numpy as np
+
+#%% Load the data
+labels = ['santa', 'not-a-santa']
+img_size = 100
+def get_data(data_dir):
+    data = [] 
+    for label in labels: 
+        path = os.path.join(data_dir, label)
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img))[...,::-1] #convert BGR to RGB format
+                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images to preferred size
+                data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data, dtype=object)
+#Now we can easily fetch our train and validation data.
+train = get_data('is that santa\\train')
+test = get_data('is that santa\\test')
+#print(train[100][0]) 
+
+#%% Show some data
+for i in range(12):
+    plt.figure()
+    plt.imshow(train[i*50][0])
+    plt.axis('off')
+    plt.title(labels[train[i*50][1]])
+
+#%%
+x_train = []
+y_train = []
+x_test = []
+y_test = []
+
+for feature, label in train:
+  x_train.append(feature)
+  y_train.append(label)
+
+for feature, label in test:
+  x_test.append(feature)
+  y_test.append(label)
+
+# Normalize the data
+x_train = np.array(x_train) / 255
+x_test = np.array(x_test) / 255
+
+x_train.reshape(-1, img_size, img_size, 1)
+y_train = np.array(y_train)
+
+x_test.reshape(-1, img_size, img_size, 1)
+y_test = np.array(y_test)
+
+#%%
+for i in range(12):
+    plt.figure()
+    plt.imshow(x_train[i*50])
+    plt.axis('off')
+    plt.title(labels[y_train[i*50]])
+    
+#%%
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range = 30,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.2, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip = True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+
+datagen.fit(x_train)
+    
+#%%
+model = Sequential()
+model.add(Conv2D(32,3,padding="same", activation="relu", input_shape=(100,100,3)))
+model.add(MaxPool2D())
+
+model.add(Conv2D(32, 3, padding="same", activation="relu"))
+model.add(MaxPool2D())
+
+model.add(Conv2D(64, 3, padding="same", activation="relu"))
+model.add(MaxPool2D())
+model.add(Dropout(0.4))
+
+model.add(Flatten())
+model.add(Dense(128,activation="relu"))
+model.add(Dense(2, activation="softmax"))
+
+model.summary()
+
+#%%
+opt = Adam(lr=0.000001)
+model.compile(optimizer = opt , loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True) , metrics = ['accuracy'])
+
+#%%
+history = model.fit(x_train,y_train,epochs = 100 , validation_data = (x_test, y_test))
+
+#%%
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(100)
+
+plt.figure(figsize=(15, 15))
+plt.subplot(2, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+#%%
+predict_x=model.predict(x_test) 
+predictions=np.argmax(predict_x,axis=1)
+
+predictions = predictions.reshape(1,-1)[0]
+print(classification_report(y_test, predictions, target_names = ['Santa (Class 0)','Not a santa (Class 1)']))
